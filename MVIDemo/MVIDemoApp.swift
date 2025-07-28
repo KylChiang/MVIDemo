81ab765eb8e9b2d1b32105432ad60d1e2f7a0c05
Kylie <chiangkaiyun@gmail.com>
2025-07-28 15:13:13 +0800

    Initial Commit

---
+++ MVIDemo/MVIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVIDemoApp.swift
+//  MVIDemo
+//
+//  Created by Kylie on 2025/7/28.
+//
+
+import SwiftUI
+
+@main
+struct MVIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVIDemoTests/MVIDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVIDemoTests.swift
+//  MVIDemoTests
+//
+//  Created by Kylie on 2025/7/28.
+//
+
+import Testing
+@testable import MVIDemo
+
+struct MVIDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
